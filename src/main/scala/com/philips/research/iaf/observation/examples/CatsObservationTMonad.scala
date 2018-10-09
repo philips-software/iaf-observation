cb93a02baf14a49d241a6ebec13cb2828a10f61e
Cliff Laschet <clifflaschet@users.noreply.github.com>
2018-10-09 22:37:46 +0200

    Finished Cats ObservationT example

--- src/main/scala/com/philips/research/iaf/observation/examples/CatsObservationTMonad.scala
+++ src/main/scala/com/philips/research/iaf/observation/examples/CatsObservationTMonad.scala
@@ -1,17 +1,16 @@
 package com.philips.research.iaf.observation.examples
 
 import cats.Monad
+import cats.implicits._
 import com.philips.research.iaf.observation.ObservationT
 
 object CatsObservationTMonad extends App{
 
-  //Example 1: Creating an Observation from Cats' Monad type class
-  import cats.instances.option.catsStdInstancesForOption
-  //ObservationT.catsMonadForObservationT(Monad[Option])
+  //Example 1: Creating an ObservationT[Option, String] using Cats' Monad type class
+  val o1 = Monad[ObservationT[Option, ?]].pure("hello!")
+  println(s"Created an ObservationT containing value ${o1.value} of type Option[Observation[String]")
 
-  val o = Monad[ObservationT[Option, ?]].pure(5)
-  println(s"Created Option[Observation[Int] $o")
-
-  val o2: ObservationT[Option, Int] = Monad[ObservationT[Option, ?]].map(o)(x => x * 2)
-  println(s"Transformed to Option[Observation[Int] $o2")
+  //Example 2: Mapping the ObservationT[Option, String] to an ObservationT[Option, Int]
+  val o2: ObservationT[Option, Int] = Monad[ObservationT[Option, ?]].map(o1)(text => text.length)
+  println(s"Transformed to an ObservationT containing value ${o2.value} of type Option[Observation[Int]")
 }
