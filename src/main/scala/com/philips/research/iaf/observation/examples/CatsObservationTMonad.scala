af6e33cbf65d888127595d50612ffef9bafc33dd
Cliff <clifflaschet@users.noreply.github.com>
2018-10-09 22:21:41 +0200

    Fixed the Cats ObservationT example

--- src/main/scala/com/philips/research/iaf/observation/examples/CatsObservationTMonad.scala
+++ src/main/scala/com/philips/research/iaf/observation/examples/CatsObservationTMonad.scala
@@ -6,11 +6,12 @@ import com.philips.research.iaf.observation.ObservationT
 object CatsObservationTMonad extends App{
 
   //Example 1: Creating an Observation from Cats' Monad type class
-  ObservationT.catsMonadForObservationT(Monad[Option])
+  import cats.instances.option.catsStdInstancesForOption
+  //ObservationT.catsMonadForObservationT(Monad[Option])
 
   val o = Monad[ObservationT[Option, ?]].pure(5)
   println(s"Created Option[Observation[Int] $o")
 
-//  val o2: ObservationT[Option, Int] = Monad[ObservationT[Option, ?]].map(o)(x => x * 2)
-//  println(s"Transformed to Option[Observation[Int] $o2")
+  val o2: ObservationT[Option, Int] = Monad[ObservationT[Option, ?]].map(o)(x => x * 2)
+  println(s"Transformed to Option[Observation[Int] $o2")
 }
